5dcd27a74a735e30922cb2c67de937bda8eae2f8
thseo <thseo@spectra.co.kr>
2023-09-17 15:33:41 +0900

    first commit

---
+++ src/main/java/com/example/matzipmap/MatzipmapApplication.java
@@ -0,0 +1,15 @@
+package com.example.matzipmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatzipmapApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MatzipmapApplication.class, args);
+    }
+
+}
